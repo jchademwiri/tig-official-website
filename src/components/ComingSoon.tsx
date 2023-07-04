64732abdf8731b4b22e1443f73419efa5c7f1521
Jacob Chademwiri <jchademwiri@gmail.com>
2023-07-04 17:16:39 +0200

    added contact details and Vercel analytics

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -13,11 +13,23 @@ const ComingSoon = () => {
         <div className='h-2 rounded-full w-full bg-slate-400 my-2'>
           <div className='h-2 rounded-full w-3/4 bg-yellow-600'></div>
         </div>
+        <h2 className='py-2 md:text-3xl text-xl font-semibold'>
+          Contact us Now
+        </h2>
+        <div className='py-2 md:py-4 grid md:grid-cols-2'>
+          <Link className='py-1' href='tel:+27796787198'>
+            +27 (0)79 678 7198
+          </Link>
+          <Link className='py-1' href='mailto:brianm@tradinginglory.co.za'>
+            brianm@tradinginglory.co.za
+          </Link>
+        </div>
       </div>
       <footer className='md:my-5 my-10 py-5'>
         <p>
           © Powered and secured by{' '}
           <Link
+            title='We Would Love to work with your business, call us now '
             href={`https://www.playhousemedia.net/`}
             className='hover:text-yellow-600 text-slate-500'
           >
