503cc828eef3778b477dbed8720dd83d697f8755
Jacob Chademwiri <jchademwiri@gmail.com>
2023-09-09 04:48:33 +0200

    upgrade v3, added header n footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
+// import vercel from '@astrojs/vercel/server';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  integrations: [tailwind()]
+  output: 'static',
+  // output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
+  integrations: [tailwind()],
 });
